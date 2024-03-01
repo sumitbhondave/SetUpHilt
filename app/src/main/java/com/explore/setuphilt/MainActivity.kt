bbb7c4e6db709304fdd34cc2d2098cfd35195e86
Shrinath <sumitbhondave@gmail.com>
2024-03-01 08:23:58 +0530

    work around to show loading indicator. -- Need to change this.

--- app/src/main/java/com/explore/setuphilt/MainActivity.kt
+++ app/src/main/java/com/explore/setuphilt/MainActivity.kt
@@ -3,8 +3,13 @@ package com.explore.setuphilt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,8 +18,10 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.explore.setuphilt.presentation.UserListScreen
 import com.explore.setuphilt.presentation.viewmodels.UserViewModel
@@ -56,5 +63,16 @@ fun UserList() {
     val userViewModel: UserViewModel = viewModel()
     val users by userViewModel.users.observeAsState(listOf())
 
-    UserListScreen(users)
+    if (users.isNotEmpty()) {
+        UserListScreen(users)
+    } else {
+        // TODO -- Need to update this logic get It from State Management
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(48.dp),
+                color = MaterialTheme.colorScheme.secondary,
+                trackColor = MaterialTheme.colorScheme.surfaceVariant,
+            )
+        }
+    }
 }
